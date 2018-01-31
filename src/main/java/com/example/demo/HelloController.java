008151bca7aafa6353107c718bb028b3d1236243
Martin Surminen <martin.surminen@comtechtel.com>
2018-01-31 17:23:22 +1100

    Changed code to use API Dropbox v2

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,10 +1,10 @@
 package com.example.demo;
 
-import java.util.Locale;
+import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,69 +12,100 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.dropbox.core.DbxAppInfo;
 import com.dropbox.core.DbxAuthFinish;
-import com.dropbox.core.DbxClient;
 import com.dropbox.core.DbxException;
 import com.dropbox.core.DbxRequestConfig;
 import com.dropbox.core.DbxSessionStore;
 import com.dropbox.core.DbxStandardSessionStore;
 import com.dropbox.core.DbxWebAuth;
-import com.dropbox.core.DbxWebAuthNoRedirect;
-import com.dropbox.core.DbxWebAuth.BadRequestException;
-import com.dropbox.core.DbxWebAuth.BadStateException;
-import com.dropbox.core.DbxWebAuth.CsrfException;
-import com.dropbox.core.DbxWebAuth.NotApprovedException;
-import com.dropbox.core.DbxWebAuth.ProviderException;
+import com.dropbox.core.DbxWebAuth.Request.Builder;
+import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.files.ListFolderErrorException;
+import com.dropbox.core.v2.files.ListFolderResult;
+import com.dropbox.core.v2.files.Metadata;
 
 @RestController
 public class HelloController {
 
-	final String APP_KEY = "pvbpqsiwyei1kxm";
-	final String APP_SECRET = "4aqz9kyp7y7s6i1";
+	final String APP_KEY = "jr0tecrty7appm4";
+	final String APP_SECRET = "ylhfjprpfvwec5s";
 
 	DbxAppInfo appInfo = new DbxAppInfo(APP_KEY, APP_SECRET);
-	DbxRequestConfig config = new DbxRequestConfig("JavaTutorial/1.0", Locale.getDefault().toString());
+	DbxRequestConfig config = new DbxRequestConfig("JavaTutorial/1.0");
 
 	DbxSessionStore csrfTokenStore;
-	
-	DbxWebAuth webAuth; 
-	
+
+	DbxWebAuth webAuth;
+
+	String redirectUri = "https://192.168.99.100:8080/demo2";
+
 	@RequestMapping("/demo")
 	public ModelAndView index(HttpServletRequest request) {
 
-		csrfTokenStore = new DbxStandardSessionStore(request.getSession(true),
-				"dropbox-auth-csrf-token");
-		webAuth = new DbxWebAuth(config, appInfo, "https://192.168.99.100:8080/demo2", csrfTokenStore);
+		csrfTokenStore = new DbxStandardSessionStore(request.getSession(true), "dropbox-auth-csrf-token");
+		// webAuth = new DbxWebAuth(config, appInfo,
+		// "https://192.168.99.100:8080/demo2", csrfTokenStore);
+		webAuth = new DbxWebAuth(config, appInfo);
 
-		String authorizeUrl = webAuth.start();
-		// authorizeUrl += "&redirect_uri=https://192.168.99.100:8080/demo2";
+		Builder builder = webAuth.newRequestBuilder();
+		builder.withRedirectUri(redirectUri, csrfTokenStore);
+
+		// String authorizeUrl = webAuth.start();
+		String authorizeUrl = webAuth.authorize(builder.build());
 
 		return new ModelAndView("redirect:" + authorizeUrl);
 	}
 
 	@RequestMapping("/demo2")
-	public String usage(HttpServletRequest request, @RequestParam("code") String code) throws DbxException,
-			BadRequestException, BadStateException, CsrfException, NotApprovedException, ProviderException {
+	public String usage(HttpServletRequest request, HttpServletResponse response, @RequestParam("code") String code)
+			throws IOException, ListFolderErrorException, DbxException {
 
-//		final DbxSessionStore csrfTokenStore = new DbxStandardSessionStore(request.getSession(true),
-//				"dropbox-auth-csrf-token");
-//		DbxWebAuth webAuth = new DbxWebAuth(config, appInfo, "https://192.168.99.100:8080/demo3", csrfTokenStore);
+		DbxAuthFinish authFinish;
 
-		// DbxAuthFinish authFinish = webAuth.finish(code);
-		DbxAuthFinish authFinish = webAuth.finish(request.getParameterMap());
-		String accessToken = authFinish.accessToken;
+		try {
+			authFinish = webAuth.finishFromRedirect(redirectUri, csrfTokenStore, request.getParameterMap());
 
-		return accessToken;
-		
-//		DbxClient client = new DbxClient(config, accessToken);
-//		String account = client.getAccountInfo().displayName;
+		} catch (
 
-//		return account;
-	}
+		DbxWebAuth.BadRequestException ex) {
+			// log("On /dropbox-auth-finish: Bad request: " + ex.getMessage());
+			response.sendError(400);
+			return ex.getMessage();
+		} catch (DbxWebAuth.BadStateException ex) {
+			// Send them back to the start of the auth flow.
+			response.sendRedirect("http://my-server.com/dropbox-auth-start");
+			return ex.getMessage();
+		} catch (DbxWebAuth.CsrfException ex) {
+			// log("On /dropbox-auth-finish: CSRF mismatch: " + ex.getMessage());
+			response.sendError(403, "Forbidden.");
+			return ex.getMessage();
+		} catch (DbxWebAuth.NotApprovedException ex) {
+			// log("User rejected: " + ex.getMessage());
+			return ex.getMessage();
+		} catch (DbxWebAuth.ProviderException ex) {
+			// log("On /dropbox-auth-finish: Auth failed: " + ex.getMessage());
+			response.sendError(503, "Error communicating with Dropbox.");
+			return ex.getMessage();
+		} catch (DbxException ex) {
+			// log("On /dropbox-auth-finish: Error getting token: " + ex.getMessage());
+			response.sendError(503, "Error communicating with Dropbox.");
+			return ex.getMessage();
+		}
 
-	@RequestMapping("/demo3")
-	public String usage2(HttpServletRequest request, @RequestParam("code") String code) throws DbxException,
-			BadRequestException, BadStateException, CsrfException, NotApprovedException, ProviderException {
+		String accessToken = authFinish.getAccessToken();
 
-		return "here";
+		DbxClientV2 client = new DbxClientV2(config, accessToken);
+
+		// CreateFolderResult create = client.files().createFolderV2("/mytest");
+		// return create.getMetadata().toString();
+
+		StringBuilder sb = new StringBuilder("Result:\n");
+		ListFolderResult listing = client.files().listFolderBuilder("/Life Log").start();
+		for (Metadata child : listing.getEntries()) {
+			sb.append(child.getName());
+			sb.append("\n");
+		}
+
+		return sb.toString();
+
 	}
 }
