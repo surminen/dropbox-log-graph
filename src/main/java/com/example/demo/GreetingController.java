9616cbe2c226f29c7b739a8bb34508a72682c7e8
Martin Surminen <martin.surminen@comtechtel.com>
2018-02-05 13:29:03 +1100

    Dropbox access and thymeleaf presentation now working

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -5,10 +5,10 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.dropbox.core.DbxAppInfo;
 import com.dropbox.core.DbxAuthFinish;
@@ -23,8 +23,8 @@ import com.dropbox.core.v2.files.ListFolderErrorException;
 import com.dropbox.core.v2.files.ListFolderResult;
 import com.dropbox.core.v2.files.Metadata;
 
-@RestController
-public class HelloController {
+@Controller
+public class GreetingController {
 
 	final String APP_KEY = "jr0tecrty7appm4";
 	final String APP_SECRET = "ylhfjprpfvwec5s";
@@ -38,26 +38,28 @@ public class HelloController {
 
 	String redirectUri = "https://192.168.99.100:8080/demo2";
 
-	@RequestMapping("/demo")
-	public ModelAndView index(HttpServletRequest request) {
+	@RequestMapping("/graph")
+	public String greeting(HttpServletRequest request) {
 
+		// Create the request objects
 		csrfTokenStore = new DbxStandardSessionStore(request.getSession(true), "dropbox-auth-csrf-token");
-		// webAuth = new DbxWebAuth(config, appInfo,
-		// "https://192.168.99.100:8080/demo2", csrfTokenStore);
 		webAuth = new DbxWebAuth(config, appInfo);
+		Builder builder = DbxWebAuth.newRequestBuilder();
 
-		Builder builder = webAuth.newRequestBuilder();
+		// Get the dropbox authorization URL
+		// Builder builder = webAuth.newRequestBuilder();
 		builder.withRedirectUri(redirectUri, csrfTokenStore);
-
-		// String authorizeUrl = webAuth.start();
 		String authorizeUrl = webAuth.authorize(builder.build());
 
-		return new ModelAndView("redirect:" + authorizeUrl);
+		return "redirect:" + authorizeUrl;
 	}
 
 	@RequestMapping("/demo2")
-	public String usage(HttpServletRequest request, HttpServletResponse response, @RequestParam("code") String code)
-			throws IOException, ListFolderErrorException, DbxException {
+	public String usage(HttpServletRequest request, HttpServletResponse response, @RequestParam("code") String code,
+			Model model) throws IOException, ListFolderErrorException, DbxException {
+
+		// Add the username to the model
+		model.addAttribute("name", "Bob");
 
 		DbxAuthFinish authFinish;
 
@@ -93,19 +95,15 @@ public class HelloController {
 
 		String accessToken = authFinish.getAccessToken();
 
+		// Get all files in the specified folder
 		DbxClientV2 client = new DbxClientV2(config, accessToken);
-
-		// CreateFolderResult create = client.files().createFolderV2("/mytest");
-		// return create.getMetadata().toString();
-
-		StringBuilder sb = new StringBuilder("Result:\n");
 		ListFolderResult listing = client.files().listFolderBuilder("/Life Log").start();
-		for (Metadata child : listing.getEntries()) {
-			sb.append(child.getName());
-			sb.append("\n");
-		}
 
-		return sb.toString();
+		// Add the file data to the model
+		model.addAttribute("data", listing.getEntries());
+
+		// return the template to display;
+		return "graphView";
 
 	}
 }
--- src/main/java/com/example/demo/UiApplication.java
+++ src/main/java/com/example/demo/UiApplication.java
@@ -2,6 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class UiApplication {
